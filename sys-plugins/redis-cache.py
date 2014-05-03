312b30a8fa81c29153657dd949fcf1d3add9ba3e
=Jeff Hickson <rvbcaboose@gmail.com>
2014-05-02 22:18:57 -0400

    Added skeleton to redis.

--- sys-plugins/redis-cache.py
+++ sys-plugins/redis-cache.py
@@ -1,3 +1,12 @@
 #Generic module to enable redis usage
 
-#import redis
+import redis
+
+class Redis_Config(object):
+	def __init__(self, host, port, db):
+		'''
+		Contains the variables to instantiate some redis connections.
+		'''
+		self.host = host
+		self.port = port
+		self.db = db
