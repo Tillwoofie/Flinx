b7727701307e4ea59777e52152a977078b7d8602
=Jeff Hickson <rvbcaboose@gmail.com>
2014-05-02 22:13:27 -0400

    Added some basic things to pgsql class.

--- sys-plugins/pgsql.py
+++ sys-plugins/pgsql.py
@@ -1,3 +1,15 @@
 #Generic module to enable database usage.
 
-#import psycoph
+import psycopg
+
+class PGSql_Config(object):
+	def __init__(self, database=None, user=None, password=None, host=None, port=None):
+		'''
+		Keeps database information for creation connections with.
+		'''
+		self.database = database
+		self.user = user
+		self.password = password
+		self.host = host
+		self.port = port
+
