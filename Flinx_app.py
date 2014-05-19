87d703abff964830161778ae39822d96d8e7ca38
=Jeff Hickson <rvbcaboose@gmail.com>
2014-05-19 19:56:43 -0400

    Refactoring for config ownership.

--- Flinx_app.py
+++ Flinx_app.py
@@ -1,11 +1,14 @@
 #Test skeleton
 
 def application(environ, start_response):
-	data = "Hello World!\nEnvironment Dump:\n"
+	import Flinx.flinx_lib.config
+	config = Flinx.flinx_lib.config.Flinx_Config()
+
+	data = "Hello World!\n\n"
 	data += "ENVIRON DUMP\n"
 	data += dump_environ(environ)
-	data += "CONFIG_DUMP\n"
-	data += dump_config()
+	data += "\nCONFIG_DUMP\n"
+	data += dump_config(config)
 	start_response("200 OK", [
 		("Content-Type", "text/plain"),
 		("Content-Length", str(len(data)))
@@ -18,12 +21,18 @@ def dump_environ(environ):
 		enc += "{}: {}\n".format(x, environ[x])
 	return enc
 
-def dump_config():
-        import Flinx.flinx_lib.config as fconf
-        config = fconf.Flinx_Config()
+def dump_config(config):
+        #import Flinx.flinx_lib.config as fconf
+        #config = fconf.Flinx_Config()
         enc = ""
         for x in config.config.keys():
                 enc += "{}: {}\n".format(x,config.config[x])
 		enc += "sys_modules: {}\n".format(config.sys_modules)
 		enc += "modules: {}\n".format(config.modules)
         return enc
+
+def route_request(environ, config):
+	'''
+	Main function to route requests into different modules.
+	'''
+	pass
