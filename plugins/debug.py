fad7de08b28203daab06d8afa4d4fa8b5311ac16
Jeff Hickson <rvbcaboose@gmail.com>
2014-05-22 15:26:07 -0400

    Had some bad references to removed variables in debug plugin still.

--- plugins/debug.py
+++ plugins/debug.py
@@ -8,9 +8,9 @@ def main(environ, parsedUrl, config):
 	data += "\nCONFIG_DUMP\n"
 	data += dump_config(config)
 	data += "\nPARSED URL DUMP\n"
-	data += dump_dict(penv.env)
+	data += dump_dict(parsedUrl.env)
 	data += "\nWSGI VARS\n"
-	data += dump_dict(penv.wsgi_var)
+	data += dump_dict(parsedUrl.wsgi_var)
 	return data
 
 def dump_dict(environ):
