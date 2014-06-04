0e458bb192c6a7197a2aede08bb5213c010d282c
Jeff Hickson <rvbcaboose@gmail.com>
2014-06-04 17:08:52 -0400

    Changed the way modules will get args, starting with redis module. Going to kwargs for easier templating.

--- sys-plugins/redis-cache.py
+++ sys-plugins/redis-cache.py
@@ -2,11 +2,14 @@
 
 import redis
 
+
 class Redis_Config(object):
-	def __init__(self, host, port, db):
+	def __init__(self, **kwargs):
 		'''
 		Contains the variables to instantiate some redis connections.
+		This class expects host, port and db config values in it's arguments, 
+		does not search for others.
 		'''
-		self.host = host
-		self.port = port
-		self.db = db
+		self.host = kwargs.get('host', 'localhost')
+		self.port = kwargs.get('port', '6379')
+		self.db = kwargs.get('db', '0')
