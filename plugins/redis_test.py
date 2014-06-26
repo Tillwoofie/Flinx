ab19b113c4d4ccd0a99f0b58f0aa7038dc278e3a
Jeff Hickson <rvbcaboose@gmail.com>
2014-06-26 16:48:08 -0400

    Oops'd an indent.

--- plugins/redis_test.py
+++ plugins/redis_test.py
@@ -10,7 +10,7 @@ def main(environ, parsedUrl, config, sys_mods):
 	r = sys_mods['redis-cache'].get_existing_connection()
 	
 	data = ""
-		LIST_SIZE = int(get_url_arg("size", parsedUrl, 100))
+	LIST_SIZE = int(get_url_arg("size", parsedUrl, 100))
 	words = generate_words_list(LIST_SIZE)
 	success, w_ins, w_get, w_del = test_list_redis(r, words)
 
