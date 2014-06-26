8fbad65f7a15735b89de5f7fda7e272e003268e9
Jeff Hickson <rvbcaboose@gmail.com>
2014-06-26 17:03:14 -0400

    Shortened the words for random generation to lower memory usage.

--- plugins/redis_test.py
+++ plugins/redis_test.py
@@ -34,7 +34,7 @@ def main(environ, parsedUrl, config, sys_mods):
 	return data
 
 def generate_words_list(length):
-	words = ["tralala", "troll", "sillyness", "apple", "banana", "thing", "hello", "python"]
+	words = ["a", "b", "c", "d", "e", "f", "g", "h"]
 	word_list = {} #using a dictionary isn't nessecary, but simple for avoiding duplicates.
 	x = 0
 	while True:
