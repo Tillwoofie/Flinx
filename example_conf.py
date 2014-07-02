690fdfbf0ae9d3e8851e682fa6486d3844c7030f
Jeff Hickson <rvbcaboose@gmail.com>
2014-07-02 12:29:18 -0400

    Updating example config.

--- example_conf.py
+++ example_conf.py
@@ -3,9 +3,26 @@
 #main conf var.
 conf = {}
 
+# what modules to load
 sys_modules = ['pgsql', 'redis-cache']
-modules = []
+modules = ['debug', 'redis_test']
 
+#postgres config
+pgsql = {}
+pgsql['user'] = 'flinx'
+pgsql['pass'] = 'a_password'
+pgsql['address'] = "localhost"
+pgsql['port'] = "5432"
+pgsql['db'] = "flinx"
+
+#redis config
+rc = {}
+rc['address'] = "localhost"
+rc["port"] = "6379"
+rc['db'] = "0"
+
+# add config to pertinent sections.
 conf['sys_modules'] = sys_modules
 conf['modules'] = modules
-
+conf['pgsql'] = pgsql
+conf['redis-cache'] = rc
