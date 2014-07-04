6198d9dff2ca893b2fd3b69e4e968d4fc41666d0
Jeff Hickson <rvbcaboose@gmail.com>
2014-07-04 11:35:26 -0400

    Adding GPL v3 licence file, and header to every file.

--- example_conf.py
+++ example_conf.py
@@ -1,5 +1,14 @@
 # Example conf for Flinx.
 
+# Written by Jeff Hickson (rvbcaboose@gmail.com).
+# This file is part of Flinx, a Python web-framework.
+
+# Flinx is free software, and comes with no warranty.
+# It is licenced under the GNU GLP v3.
+# The full terms of this are available in the LICENCE file,
+# in the root of the project, at <http://www.gnu.org/licenses/>,
+# or at <https://github.com/Tillwoofie/Flinx/blob/master/LICENCE>
+
 #main conf var.
 conf = {}
 
--- plugins/example.py
+++ plugins/example.py
@@ -1,3 +1,13 @@
+
+# Written by Jeff Hickson (rvbcaboose@gmail.com).
+# This file is part of Flinx, a Python web-framework.
+
+# Flinx is free software, and comes with no warranty.
+# It is licenced under the GNU GLP v3.
+# The full terms of this are available in the LICENCE file,
+# in the root of the project, at <http://www.gnu.org/licenses/>,
+# or at <https://github.com/Tillwoofie/Flinx/blob/master/LICENCE>
+
 #Basic example.
 
 
